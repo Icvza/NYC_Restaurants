10bb852ea1a547c2dbe55ad366b4ad13319c79d6
Isaac Chavez <devisaacalejandro@gmail.com>
2020-12-29 00:02:13 -0500

    re-did option 1 in CLI and added detail method

--- lib/nyc_restaurant_v/cli.rb
+++ lib/nyc_restaurant_v/cli.rb
@@ -3,23 +3,24 @@ class CLI
  
     def start 
         blankspace
-        puts "Hello foodie! This is a tool that lets you know a restaurant's grade and violation's."
+        puts "Hello foodie! This is a tool that lets you know a restaurant's grade and violation's located in New York City."
         blankspace
         API.get_data
         main_menu
     end
 
     def main_menu
+        puts "To see a list of all restaurants type List "
         blankspace
-        puts "To see a list of all restaurants enter 1 "
-        puts "To search for a restaurant by name enter 2"
+        puts "To search for a restaurant by name enter Search"
+        blankspace
         puts "To leave type exit"
         blankspace
         selection = user_input
 
-        if selection == "1"
+        if selection == "List"
             option_1
-        elsif selection == "2"
+        elsif selection == "search"
             option_2
         elsif selection == "exit"
             goodbye
@@ -29,17 +30,42 @@ class CLI
     end
 
     def option_1
+        blankspace
         restaurant_list
         blankspace
-        puts "TYPE IN THE NUMBER NEXT TO THE RESTURANT TO VIEW ITS FULL PROFILE OTHERWISE TYPE back FOR MAIN MENU"
+        puts "TYPE IN THE NUMBER NEXT TO THE RESTURANT TO VIEW ITS FULL PROFILE OTHERWISE TYPE 0 FOR MAIN MENU"
         blankspace
         selection = user_input
-        if selection == "back"
+        @input = selection.to_i 
+        if @input == 0
+            puts "Taking you back to the main menu either because you entered 0 or your typed something invalid" 
             main_menu
-        elsif selection =="x"
-            restaurant_info
-        else
-            invalid
+        else @input >= 1
+            find_by_index(@input)
+        end 
+        blankspace
+        main_menu
+    end
+
+    def find_by_index(input)
+        @selected_restaurant = []
+        @selected_restaurant << Restaurant.all[input-1] 
+        restaurant_details(@selected_restaurant)
+    end
+
+    def restaurant_details(restaurant)
+        blankspace
+        restaurant.each do |attribute|
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+            puts "Name: #{attribute.dba}"
+            blankspace
+            puts "Street: #{attribute.street}"
+            blankspace
+            puts "Grade: #{attribute.grade}"
+            blankspace
+            puts "Violation: #{attribute.violation_description}"
+            blankspace
+            puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
         end
     end
 
@@ -48,11 +74,6 @@ class CLI
             puts "#{i+1} - #{resturant.dba} - #{resturant.street}"
         end
     end 
-
-    def restaurant_info
-        puts "LETS GET IT STARTED IN HERE"
-        binding.pry
-    end
  
     def user_input
         gets.strip
@@ -76,5 +97,5 @@ class CLI
         puts"Till next meal, goodbye!"
         blankspace
     end
-
 end
+
